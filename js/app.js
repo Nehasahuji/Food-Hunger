1b4a4f5f6fd5fcfbd4de9d81834b71023cabcee6
Neha Sahu <nehasahu9424@gmail.com>
2021-10-12 11:09:14 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.foot-slider').slick({
+          autoplay:true,
+          slidesToShow:3,
+          slidesToScrill:1,
+          prevArrow:".prev-btn",
+          nextArrow:".next-btn",
+
+
+    });
+  });
